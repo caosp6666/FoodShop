876018a01bf515af35dc88421fd4f722ebee45aa
Albert Cao <61658007+caosp6666@users.noreply.github.com>
2020-06-30 11:17:28 +0800

    Update settings.py

--- MyFoodshop/settings.py
+++ MyFoodshop/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '=o25kkrvg4xnb*6%a=rdxhx!cu%mt*n28vp7jw69q-!n5#*o$e'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
