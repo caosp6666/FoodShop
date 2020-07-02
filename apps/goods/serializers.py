e6685dd1e46837897d426eaf67e8b8f69f6200eb
caosp6666 <caosp6666@163.com>
2020-07-02 14:23:34 +0800

    ListAPIView的强大

--- apps/goods/serializers.py
+++ apps/goods/serializers.py
@@ -4,9 +4,27 @@
 # @Software: PyCharm
 
 from rest_framework import serializers
-from .models import Goods
+from .models import Goods, GoodsCategory
 
 
-class GoodsSerializer(serializers.Serializer):
-    name = serializers.CharField(required=True, max_length=100)
-    click_num = serializers.IntegerField(default=0)
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = GoodsCategory
+        fields = "__all__"
+
+
+# class GoodsSerializer(serializers.Serializer):
+#     name = serializers.CharField(required=True, max_length=100)
+#     click_num = serializers.IntegerField(default=0)
+#     goods_front_image = serializers.ImageField()
+#
+#     def create(self, validated_data):
+#         return Goods.objects.create(**validated_data)
+
+
+class GoodsSerializer(serializers.ModelSerializer):
+    category = CategorySerializer()
+    class Meta:
+        model = Goods
+        # fields = ('name', 'click_num', 'shop_price', 'goods_front_image', 'add_time')
+        fields = "__all__"
--- apps/goods/views.py
+++ apps/goods/views.py
@@ -1,11 +1,15 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+from rest_framework import mixins
+from rest_framework import generics
+
 
 from .models import Goods
 from .serializers import GoodsSerializer
 
 
-class GoodsListView(APIView):
+class GoodsListAPIView(APIView):
     """
     list all goods
     """
@@ -13,3 +17,26 @@ class GoodsListView(APIView):
         goods = Goods.objects.all()[:10]
         goods_serializer = GoodsSerializer(goods, many=True)  # 因为是goods是list，多条数据要增加many=True
         return Response(goods_serializer.data)
+
+    # def post(self, request, format=None):
+    #     serializer = GoodsSerializer(data=request.data)
+    #     if serializer.is_valid():
+    #         serializer.save()
+    #         return Response(serializer.data, status=status.HTTP_201_CREATED)
+    #     else:
+    #         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+# class GoodsListView(mixins.ListModelMixin, generics.GenericAPIView):
+#     queryset = Goods.objects.all()[:10]
+#     serializer_class = GoodsSerializer
+#
+#     def get(self, request, *args, **kwargs):
+#         return self.list(request, *args, **kwargs)
+#     # 如果不重写这个get，会返回：get方法不允许
+#     # 以上操作就相当于ListAPIView
+
+
+class GoodsListView(generics.ListAPIView):
+    queryset = Goods.objects.all()[:10]
+    serializer_class = GoodsSerializer
