5eee982dfba04f04368cea332f4d258a1eaabeff
caosp6666 <caosp6666@163.com>
2020-07-02 11:04:04 +0800

    添加三种方式比较

--- apps/goods/view_django.py
+++ apps/goods/view_django.py
@@ -17,13 +17,35 @@ class GoodsListView(View):
         """
         json_list = []
         goods = Goods.objects.all()[:10]
-        for good in goods:
-            json_dict = {}
-            json_dict["name"] = good.name
-            json_dict["category"] = good.category.name
-            json_dict["market_price"] = good.market_price
-            json_list.append(json_dict)
 
-        from django.http import HttpResponse
+        # 方法一：使用手动添加
+        # for good in goods:
+        #     json_dict = {}
+        #     json_dict["name"] = good.name
+        #     json_dict["category"] = good.category.name
+        #     json_dict["market_price"] = good.market_price
+        #     # json_dict["add_time"] = good.add_time  # 这个字段无法通过json.dumps序列化
+        #     json_list.append(json_dict)
+        # from django.http import HttpResponse
+        # import json
+        # return HttpResponse(json.dumps(json_list), content_type="application/json")
+
+        # 方法二：使用model_to_dict，同样会有字段不能序列化的问题
+        # from django.forms.models import model_to_dict
+        # for good in goods:
+        #     json_dict = model_to_dict(good)
+        #     json_list.append(json_dict)
+        # from django.http import HttpResponse
+        # import json
+        # return HttpResponse(json.dumps(json_list), content_type="application/json")
+
+        # 方法三：使用django的serializers进行序列化
         import json
-        return HttpResponse(json.dumps(json_list), content_type="application/json")
+        from django.core import serializers
+        json_data = serializers.serialize("json", goods)
+        json_data = json.loads(json_data)
+        from django.http import JsonResponse
+        return JsonResponse(json_data, safe=False)
+
+
+
